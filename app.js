c7bc944cb2555df77c117f4a690752f9ee97abcf
Ali Alperen Arıkan <alperen.arikan95@gmail.com>
2020-10-03 17:23:11 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 document.addEventListener("DOMContentLoaded",addOptions);
 const movieOrder = 0;
+
+
 function addOptions(){
     let startingYear = 1900;
     let currentYear = new Date().getFullYear();
@@ -22,7 +24,6 @@ document.getElementById("movie-information").addEventListener("submit",function 
     const year = "&y="+movieYear;
     const type = "&type="+movieSeries;
 
-    console.log(movieTitle);
 fetch(`https://www.omdbapi.com/?apikey=1de2b15&s=${title}${year}${type}`)
 .then(response =>{
     if(!response.ok){
@@ -32,13 +33,15 @@ fetch(`https://www.omdbapi.com/?apikey=1de2b15&s=${title}${year}${type}`)
     return response.json();
 })
 .then(data =>{
+    movieListGenerator();
     document.getElementById("movie-list").innerHTML="";
 
-    for(i = movieOrder; i<movieOrder+10; i++){
+    for(i = movieOrder; i<movieOrder+15; i++){
         const titleData = data.Search[i].Title;
         const posterData = data.Search[i].Poster;
         const typeData = data.Search[i].Type;
         const yearData = data.Search[i].Year;
+        const imdbId = data.Search[i].imdbID;
 
         console.log(titleData);
         document.getElementById("movie-list").innerHTML +=
@@ -49,6 +52,7 @@ fetch(`https://www.omdbapi.com/?apikey=1de2b15&s=${title}${year}${type}`)
         <td>${titleData}</td>
         <td>${typeData}</td>
         <td>${yearData}</td>
+        <td><a href="https://www.imdb.com/title/${imdbId}" target="_Blank">IMDB Page</a>
       </tr>
         
         
@@ -59,3 +63,34 @@ event.preventDefault();
 });
 
 
+function movieListGenerator(){
+    const movieList = document.getElementById("movie-list-container");
+
+    movieList.innerHTML =
+    `
+    
+    <div class="row">
+    <div class="column">
+      <table>
+          <thead>
+            <tr>
+              <th>Poster</th>
+              <th>Title</th>
+              <th>Type</th>
+              <th>Year</th>
+              <th>IMDB Page</th>
+            </tr>
+          </thead>
+          <tbody id="movie-list">
+
+
+          </tbody>
+        </table>
+
+    </div>
+  </div>
+    
+    `;
+
+
+};
